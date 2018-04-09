2e700a7d7a27ab7f85dd85c0649312bc9b6633c7
BR\bdyp <romuloregis.sonda@br-petrobras.com.br>
2018-04-09 19:19:30 -0300

    Aula 93 Rotas com Wildcard (Criando Página Não Encontrada)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 //import { CoreModule } from './core/core.module';
 
 @NgModule({
@@ -34,7 +35,8 @@ import { SharedModule } from './shared/shared.module';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderSummaryComponent
+    OrderSummaryComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,

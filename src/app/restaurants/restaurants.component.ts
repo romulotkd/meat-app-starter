c501d0fd469d67d41a585ad3be3920fb9a9e452c
BR\bdyp <romuloregis.sonda@br-petrobras.com.br>
2018-04-10 17:01:10 -0300

    Aula 103 Aplicando Animações na Barra de Busca

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantsService } from './restaurants.service';
+import { trigger, state, style, transition, animate } from '@angular/animations'
+
 
 @Component({
   selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+  templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        "max-height":"0px"
+      })),
+      state('visible', style({
+        opacity: 1,
+        "max-height":"70px",
+        "margin-top":"20px"
+      })),
+      transition('* => *', animate('250ms 0ms ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
   restaurants: Restaurant[]
+
+  searchBarState = 'hidden'
 
   constructor(private restaurantService : RestaurantsService) { }
 
@@ -16,4 +34,7 @@ export class RestaurantsComponent implements OnInit {
     this.restaurantService.restaurants().subscribe(restaurants => this.restaurants = restaurants);
   }
 
+  toggleSearch() {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden'
+  }
 }
